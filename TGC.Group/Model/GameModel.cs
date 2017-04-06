75c61499e769cb88253359bc7ad4f26d989a14d1
leand <leand@DESKTOP-IBAC4I3>
2017-04-05 23:49:31 -0300

    Se agrega 1 piso extra (YAY! ^_^), y puertas para el mismo TODO escaleras y elementos extras

--- TGC.Group/Model/GameModel.cs
+++ TGC.Group/Model/GameModel.cs
@@ -56,6 +56,20 @@ namespace TGC.Group.Model
         private TgcMesh Puerta12 { get; set; }
         private TgcMesh Puerta13 { get; set; }
         private TgcMesh Puerta14 { get; set; }
+        private TgcMesh Puerta15 { get; set; }
+        private TgcMesh Puerta16 { get; set; }
+        private TgcMesh Puerta17 { get; set; }
+        private TgcMesh Puerta18 { get; set; }
+        private TgcMesh Puerta19 { get; set; }
+        private TgcMesh Puerta20 { get; set; }
+        private TgcMesh Puerta21 { get; set; }
+        private TgcMesh Puerta22 { get; set; }
+        private TgcMesh Puerta23 { get; set; }
+        private TgcMesh Puerta24 { get; set; }
+        private TgcMesh Puerta25 { get; set; }
+        private TgcMesh Puerta26 { get; set; }
+        private TgcMesh Puerta27 { get; set; }
+        private TgcMesh Puerta28 { get; set; }
         //Boleano para ver si dibujamos el boundingbox
         private bool BoundingBox { get; set; }
 
@@ -74,8 +88,8 @@ namespace TGC.Group.Model
 
             //Version para cargar escena desde carpeta descomprimida
             TgcSceneLoader loader = new TgcSceneLoader();
-            TgcScene = loader.loadSceneFromFile(this.MediaDir + "Level1-TgcScene.xml", this.MediaDir + "\\");
-
+            TgcScene = loader.loadSceneFromFile(this.MediaDir + "FullLevel-TgcScene.xml", this.MediaDir + "\\");
+            
             //Device de DirectX para crear primitivas.
             //
             PuertaModelo = loader.loadSceneFromFile(this.MediaDir + "\\PUERTA2-TgcScene.xml").Meshes[0];
@@ -143,6 +157,69 @@ namespace TGC.Group.Model
             Puerta14 = PuertaModelo.createMeshInstance("Puerta14");
             Puerta14.AutoTransformEnable = true;
             Puerta14.move(89f, 32f, 922f);
+
+            Puerta15 = PuertaModelo.createMeshInstance("Puerta1");
+            Puerta15.AutoTransformEnable = true;
+            Puerta15.move(89f, 142f, 275f);
+
+            Puerta16 = PuertaModelo.createMeshInstance("Puerta2");
+            Puerta16.AutoTransformEnable = true;
+            Puerta16.move(439f, 142f, 203f);
+
+            Puerta17 = PuertaModelo.createMeshInstance("Puerta3");
+            Puerta17.AutoTransformEnable = true;
+            Puerta17.move(201f, 142f, 1570f);
+            Puerta17.rotateY(FastMath.PI_HALF);
+
+            Puerta18 = PuertaModelo.createMeshInstance("Puerta4");
+            Puerta18.AutoTransformEnable = true;
+            Puerta18.move(452f, 142f, 1221f);
+            Puerta18.rotateY(FastMath.PI_HALF);
+
+            Puerta19 = PuertaModelo.createMeshInstance("Puerta5");
+            Puerta19.AutoTransformEnable = true;
+            Puerta19.move(459f, 142f, 1675f);
+
+            Puerta20 = PuertaModelo.createMeshInstance("Puerta6");
+            Puerta20.AutoTransformEnable = true;
+            Puerta20.move(734f, 142f, 1570f);
+            Puerta20.rotateY(FastMath.PI_HALF);
+
+            Puerta21 = PuertaModelo.createMeshInstance("Puerta7");
+            Puerta21.AutoTransformEnable = true;
+            Puerta21.move(915f, 142f, 751f);
+            Puerta21.rotateY(FastMath.PI_HALF);
+
+            Puerta22 = PuertaModelo.createMeshInstance("Puerta8");
+            Puerta22.AutoTransformEnable = true;
+            Puerta22.move(695f, 142f, 600f);
+            Puerta22.rotateY(FastMath.PI_HALF);
+
+            Puerta23 = PuertaModelo.createMeshInstance("Puerta9");
+            Puerta23.AutoTransformEnable = true;
+            Puerta23.move(469f, 142f, 921f);
+
+            Puerta24 = PuertaModelo.createMeshInstance("Puerta10");
+            Puerta24.AutoTransformEnable = true;
+            Puerta24.move(695f, 142f, 912f);
+            Puerta24.rotateY(FastMath.PI_HALF);
+
+            Puerta25 = PuertaModelo.createMeshInstance("Puerta11");
+            Puerta25.AutoTransformEnable = true;
+            Puerta25.move(399f, 142f, 724f);
+
+            Puerta26 = PuertaModelo.createMeshInstance("Puerta12");
+            Puerta26.AutoTransformEnable = true;
+            Puerta26.move(454f, 142f, 331f);
+            Puerta26.rotateY(FastMath.PI_HALF);
+
+            Puerta27 = PuertaModelo.createMeshInstance("Puerta13");
+            Puerta27.AutoTransformEnable = true;
+            Puerta27.move(399f, 142f, 1292f);
+
+            Puerta28 = PuertaModelo.createMeshInstance("Puerta14");
+            Puerta28.AutoTransformEnable = true;
+            Puerta28.move(89f, 142f, 922f);
             //Textura de la carperta Media. Game.Default es un archivo de configuracion (Game.settings) util para poner cosas.
             //Pueden abrir el Game.settings que se ubica dentro de nuestro proyecto para configurar.
             var pathTexturaCaja = MediaDir + Game.Default.TexturaCaja;
@@ -217,6 +294,21 @@ namespace TGC.Group.Model
             Puerta12.render();
             Puerta13.render();
             Puerta14.render();
+            Puerta15.render();
+            Puerta16.render();
+            Puerta17.render();
+            Puerta18.render();
+            Puerta19.render();
+            Puerta20.render();
+            Puerta21.render();
+            Puerta22.render();
+            Puerta23.render();
+            Puerta24.render();
+            Puerta25.render();
+            Puerta26.render();
+            Puerta27.render();
+            Puerta28.render();
+            
             //Finaliza el render y presenta en pantalla, al igual que el preRender se debe para casos puntuales es mejor utilizar a mano las operaciones de EndScene y PresentScene
             PostRender();
         }
