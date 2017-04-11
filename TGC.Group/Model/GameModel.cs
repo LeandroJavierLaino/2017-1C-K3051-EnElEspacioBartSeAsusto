dfd03d7d42a41086a419752328801098a33b061f
leand <leand@DESKTOP-IBAC4I3>
2017-04-10 23:26:46 -0300

    se agrega 1 luz que se mueve junto con la camara y se corrige la posicion inicial de la camara

--- TGC.Group/Model/GameModel.cs
+++ TGC.Group/Model/GameModel.cs
@@ -1,5 +1,6 @@
 using Microsoft.DirectX;
 using Microsoft.DirectX.DirectInput;
+using Microsoft.DirectX.Direct3D;
 using System.Drawing;
 using TGC.Core.Direct3D;
 using TGC.Core.Example;
@@ -10,6 +11,8 @@ using TGC.Core.SceneLoader;
 using TGC.Core.Textures;
 using TGC.Core.Utils;
 using System.Collections.Generic;
+using TGC.Core.UserControls.Modifier;
+using TGC.Core.Shaders;
 
 namespace TGC.Group.Model
 {
@@ -72,6 +75,10 @@ namespace TGC.Group.Model
         private TgcMesh Puerta28 { get; set; }
         //Boleano para ver si dibujamos el boundingbox
         private bool BoundingBox { get; set; }
+        
+        private Microsoft.DirectX.Direct3D.Effect Shader { get; set; }
+        private TgcBox lightMesh;
+         
 
         /// <summary>
         ///     Se llama una sola vez, al principio cuando se ejecuta el ejemplo.
@@ -83,7 +90,7 @@ namespace TGC.Group.Model
         {
             //FPS Camara Modo Dios 
             //TODO: diseñar camara con colisiones y física.
-            Camara = new Examples.Camara.TgcFpsCamera(new Vector3(0f, 0f, 0f), 100f, 100f, Input);
+            Camara = new Examples.Camara.TgcFpsCamera(new Vector3(463, 51, 83), 100f, 100f, Input);
             var d3dDevice = D3DDevice.Instance.Device;
 
             //Version para cargar escena desde carpeta descomprimida
@@ -98,128 +105,156 @@ namespace TGC.Group.Model
             Puerta1 = PuertaModelo.createMeshInstance("Puerta1");
             Puerta1.AutoTransformEnable = true;
             Puerta1.move(89f, 31.5f, 275f);
+            TgcScene.Meshes.Add(Puerta1);
 
             Puerta2 = PuertaModelo.createMeshInstance("Puerta2");
             Puerta2.AutoTransformEnable = true;
             Puerta2.move(439f, 32f, 203f);
+            TgcScene.Meshes.Add(Puerta2);
 
             Puerta3 = PuertaModelo.createMeshInstance("Puerta3");
             Puerta3.AutoTransformEnable = true;
             Puerta3.move(201f, 32f, 1570f);
             Puerta3.rotateY(FastMath.PI_HALF);
+            TgcScene.Meshes.Add(Puerta3);
 
             Puerta4 = PuertaModelo.createMeshInstance("Puerta4");
             Puerta4.AutoTransformEnable = true;
             Puerta4.move(452f, 32f, 1221f);
             Puerta4.rotateY(FastMath.PI_HALF);
+            TgcScene.Meshes.Add(Puerta4);
 
             Puerta5 = PuertaModelo.createMeshInstance("Puerta5");
             Puerta5.AutoTransformEnable = true;
             Puerta5.move(459f, 32f, 1675f);
+            TgcScene.Meshes.Add(Puerta5);
 
             Puerta6 = PuertaModelo.createMeshInstance("Puerta6");
             Puerta6.AutoTransformEnable = true;
             Puerta6.move(734f, 32f, 1570f);
             Puerta6.rotateY(FastMath.PI_HALF);
+            TgcScene.Meshes.Add(Puerta6);
 
             Puerta7 = PuertaModelo.createMeshInstance("Puerta7");
             Puerta7.AutoTransformEnable = true;
             Puerta7.move(915f, 32f, 751f);
             Puerta7.rotateY(FastMath.PI_HALF);
+            TgcScene.Meshes.Add(Puerta7);
 
             Puerta8 = PuertaModelo.createMeshInstance("Puerta8");
             Puerta8.AutoTransformEnable = true;
             Puerta8.move(695f, 32f, 600f);
             Puerta8.rotateY(FastMath.PI_HALF);
+            TgcScene.Meshes.Add(Puerta8);
 
             Puerta9 = PuertaModelo.createMeshInstance("Puerta9");
             Puerta9.AutoTransformEnable = true;
             Puerta9.move(469f, 32f, 921f);
+            TgcScene.Meshes.Add(Puerta9);
 
             Puerta10 = PuertaModelo.createMeshInstance("Puerta10");
             Puerta10.AutoTransformEnable = true;
             Puerta10.move(695f, 32f, 912f);
             Puerta10.rotateY(FastMath.PI_HALF);
+            TgcScene.Meshes.Add(Puerta10);
 
             Puerta11 = PuertaModelo.createMeshInstance("Puerta11");
             Puerta11.AutoTransformEnable = true;
             Puerta11.move(399f, 32f, 724f);
+            TgcScene.Meshes.Add(Puerta11);
 
             Puerta12 = PuertaModelo.createMeshInstance("Puerta12");
             Puerta12.AutoTransformEnable = true;
             Puerta12.move(454f, 32f, 331f);
             Puerta12.rotateY(FastMath.PI_HALF);
+            TgcScene.Meshes.Add(Puerta12);
 
             Puerta13 = PuertaModelo.createMeshInstance("Puerta13");
             Puerta13.AutoTransformEnable = true;
             Puerta13.move(399f, 32f, 1292f);
+            TgcScene.Meshes.Add(Puerta13);
 
             Puerta14 = PuertaModelo.createMeshInstance("Puerta14");
             Puerta14.AutoTransformEnable = true;
             Puerta14.move(89f, 32f, 922f);
+            TgcScene.Meshes.Add(Puerta14);
 
             Puerta15 = PuertaModelo.createMeshInstance("Puerta1");
             Puerta15.AutoTransformEnable = true;
             Puerta15.move(89f, 142f, 275f);
+            TgcScene.Meshes.Add(Puerta15);
 
             Puerta16 = PuertaModelo.createMeshInstance("Puerta2");
             Puerta16.AutoTransformEnable = true;
             Puerta16.move(439f, 142f, 203f);
+            TgcScene.Meshes.Add(Puerta16);
 
             Puerta17 = PuertaModelo.createMeshInstance("Puerta3");
             Puerta17.AutoTransformEnable = true;
             Puerta17.move(201f, 142f, 1570f);
             Puerta17.rotateY(FastMath.PI_HALF);
+            TgcScene.Meshes.Add(Puerta17);
 
             Puerta18 = PuertaModelo.createMeshInstance("Puerta4");
             Puerta18.AutoTransformEnable = true;
             Puerta18.move(452f, 142f, 1221f);
             Puerta18.rotateY(FastMath.PI_HALF);
+            TgcScene.Meshes.Add(Puerta18);
 
             Puerta19 = PuertaModelo.createMeshInstance("Puerta5");
             Puerta19.AutoTransformEnable = true;
             Puerta19.move(459f, 142f, 1675f);
+            TgcScene.Meshes.Add(Puerta19);
 
             Puerta20 = PuertaModelo.createMeshInstance("Puerta6");
             Puerta20.AutoTransformEnable = true;
             Puerta20.move(734f, 142f, 1570f);
             Puerta20.rotateY(FastMath.PI_HALF);
+            TgcScene.Meshes.Add(Puerta20);
 
             Puerta21 = PuertaModelo.createMeshInstance("Puerta7");
             Puerta21.AutoTransformEnable = true;
             Puerta21.move(915f, 142f, 751f);
             Puerta21.rotateY(FastMath.PI_HALF);
+            TgcScene.Meshes.Add(Puerta21);
 
             Puerta22 = PuertaModelo.createMeshInstance("Puerta8");
             Puerta22.AutoTransformEnable = true;
             Puerta22.move(695f, 142f, 600f);
             Puerta22.rotateY(FastMath.PI_HALF);
+            TgcScene.Meshes.Add(Puerta22);
 
             Puerta23 = PuertaModelo.createMeshInstance("Puerta9");
             Puerta23.AutoTransformEnable = true;
             Puerta23.move(469f, 142f, 921f);
+            TgcScene.Meshes.Add(Puerta23);
 
             Puerta24 = PuertaModelo.createMeshInstance("Puerta10");
             Puerta24.AutoTransformEnable = true;
             Puerta24.move(695f, 142f, 912f);
             Puerta24.rotateY(FastMath.PI_HALF);
+            TgcScene.Meshes.Add(Puerta24);
 
             Puerta25 = PuertaModelo.createMeshInstance("Puerta11");
             Puerta25.AutoTransformEnable = true;
             Puerta25.move(399f, 142f, 724f);
+            TgcScene.Meshes.Add(Puerta25);
 
             Puerta26 = PuertaModelo.createMeshInstance("Puerta12");
             Puerta26.AutoTransformEnable = true;
             Puerta26.move(454f, 142f, 331f);
             Puerta26.rotateY(FastMath.PI_HALF);
+            TgcScene.Meshes.Add(Puerta26);
 
             Puerta27 = PuertaModelo.createMeshInstance("Puerta13");
             Puerta27.AutoTransformEnable = true;
             Puerta27.move(399f, 142f, 1292f);
+            TgcScene.Meshes.Add(Puerta27);
 
             Puerta28 = PuertaModelo.createMeshInstance("Puerta14");
             Puerta28.AutoTransformEnable = true;
             Puerta28.move(89f, 142f, 922f);
+            TgcScene.Meshes.Add(Puerta28);
             //Textura de la carperta Media. Game.Default es un archivo de configuracion (Game.settings) util para poner cosas.
             //Pueden abrir el Game.settings que se ubica dentro de nuestro proyecto para configurar.
             var pathTexturaCaja = MediaDir + Game.Default.TexturaCaja;
@@ -236,6 +271,15 @@ namespace TGC.Group.Model
             //Entonces actualizamos la posición lógica, luego podemos utilizar esto en render para posicionar donde corresponda con transformaciones.
             Box.Position = new Vector3(-25, 0, 0);
 
+            //Luz??
+
+            lightMesh = TgcBox.fromSize(new Vector3(10, 10, 10));
+
+            //Pongo al mesh en posicion, activo e AutoTransform
+            lightMesh.AutoTransformEnable = true;
+            lightMesh.Position = new Vector3(463, 51, 83);
+            lightMesh.Color = Color.GreenYellow;
+                      
         }
 
         /// <summary>
@@ -263,6 +307,53 @@ namespace TGC.Group.Model
         {
             //Inicio el render de la escena, para ejemplos simples. Cuando tenemos postprocesado o shaders es mejor realizar las operaciones según nuestra conveniencia.
             PreRender();
+            var hayLuz = true;
+            if (hayLuz)
+            {
+                Shader = TgcShaders.Instance.TgcMeshPointLightShader;
+            }
+            else
+            {
+                Shader = TgcShaders.Instance.TgcMeshShader;
+            }
+
+            foreach (var mesh in TgcScene.Meshes)
+            {
+                mesh.Effect = Shader;
+                mesh.Technique = TgcShaders.Instance.getTgcMeshTechnique(mesh.RenderType);
+            }
+
+            lightMesh.Position = Camara.Position;
+
+            var lightDir = new Vector3(1,0,0);
+            lightDir.Normalize();
+
+            //Renderizar meshes
+            foreach (var mesh in TgcScene.Meshes)
+            {
+                if (hayLuz)
+                {
+                    //Cargar variables shader de la luz
+                    mesh.Effect.SetValue("lightColor", ColorValue.FromColor(lightMesh.Color));
+                    mesh.Effect.SetValue("lightPosition", TgcParserUtils.vector3ToFloat4Array(Camara.Position));
+                    mesh.Effect.SetValue("eyePosition", TgcParserUtils.vector3ToFloat4Array(Camara.Position));
+                    //mesh.Effect.SetValue("spotLightDir", TgcParserUtils.vector3ToFloat3Array(lightDir));
+                    mesh.Effect.SetValue("lightIntensity", 15f);
+                    mesh.Effect.SetValue("lightAttenuation", 2f);
+                    //mesh.Effect.SetValue("spotLightAngleCos", 39f);
+                    //mesh.Effect.SetValue("spotLightExponent", 7f);
+
+                    //Cargar variables de shader de Material. El Material en realidad deberia ser propio de cada mesh. Pero en este ejemplo se simplifica con uno comun para todos
+                    mesh.Effect.SetValue("materialEmissiveColor", ColorValue.FromColor(Color.Black));
+                    mesh.Effect.SetValue("materialAmbientColor", ColorValue.FromColor(Color.White));
+                    mesh.Effect.SetValue("materialDiffuseColor", ColorValue.FromColor(Color.White));
+                    mesh.Effect.SetValue("materialSpecularColor", ColorValue.FromColor(Color.White));
+                    mesh.Effect.SetValue("materialSpecularExp", 9f);
+                }
+
+                //Renderizar modelo
+                mesh.render();
+            }
 
             //Dibuja un texto por pantalla
             DrawText.drawText("Use W,A,S,D para desplazarte, Espacio para subir verticalmente, Control para bajar verticalmente y el mouse para mover la camara: " + TgcParserUtils.printVector3(Camara.Position) + TgcParserUtils.printVector3(Camara.LookAt) + "\n", 0, 30, Color.OrangeRed);
@@ -278,7 +369,9 @@ namespace TGC.Group.Model
             //Es útil cuando tenemos transformaciones simples, pero OJO cuando tenemos transformaciones jerárquicas o complicadas.
             //Mesh.UpdateMeshTransform();
             //Render de una escena
-            TgcScene.renderAll();
+            //TgcScene.renderAll();
+
+            //lightMesh.render();
 
             Puerta1.render();
             Puerta2.render();
@@ -337,6 +430,7 @@ namespace TGC.Group.Model
             Puerta13.dispose();
             Puerta14.dispose();
             TgcScene.disposeAll();
+            Shader.Dispose();
         }
     }
 }
